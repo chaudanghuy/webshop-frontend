4533f4a5ebb40bd12faf9f40cdf7238536674eb8
Huy Chau <chaudanghuy@gmail.com>
2025-02-08 10:18:21 +0700

    Update apiRequest.js

--- src/lib/apiRequest.js
+++ src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "http://localhost:8800/api",
+  baseURL: "https://webshop-backend-8c5o.onrender.com/api",
   withCredentials: true,
 });
 
