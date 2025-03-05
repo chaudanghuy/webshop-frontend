3683d3fbc1e973dc7f16f42271360d96b39c8bcd
Huy Chau <chaudanghuy@gmail.com>
2025-03-05 23:09:33 +0700

    Update start-app.bat
    Update start-app.bat
    
    Update start-app.bat
    
    Update start-app.bat
    
    Update start-app.bat

--- start-app.bat
+++ start-app.bat
@@ -1,12 +1,5 @@
 @echo off
-echo Fetching latest code...
-git fetch --all
-git reset --hard origin/main
 git pull origin main
-echo Installing dependencies...
-npm install
-echo Restarting development server...
-taskkill /F /IM node.exe 
 start cmd /k "npm run dev"
-echo Update completed!
-pause
+timeout /t 2
+start http://localhost:3000
